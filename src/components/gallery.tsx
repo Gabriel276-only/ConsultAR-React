d7199256acc22e1fb9bb667adc7d82d73f945108
Gabriel276-only <gabrielmagaborges@gmail.com>
2026-01-19 18:29:15 -0300

    rename public/assets/professional/Acácio borges.png to public/assets/professional/profissional1.png rename public/assets/professional/PSI Regiane Magalhães.png to public/assets/professional/profissional2.png rename public/assets/professional/PSI Bárbara Crivas.png to public/assets/professional/profissional3.png rename public/assets/professional/PSI Ana Cláudia.png to public/assets/professional/profissional4.png rename public/assets/professional/PSI Raabe Santos.png to public/assets/professional/profissional5.png rename public/assets/professional/FISIO Andressa Flor.png to public/assets/professional/profissional6.png new file public/assets/professional/profissional7.png modify src/components/gallery.tsx
    include the following commit message:
    Update professional images and gallery component

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -29,37 +29,43 @@ const Galeria = () => {
   const slidesData = [
     {
       title: "Massoterapeuta Acácio Fonseca Borges",
-      image: "/assets/professional/Acácio borges.png",
+      image: "/assets/professional/profissional1.png",
       description:
         "Desenvolvo protocolos para promoção de Saúde e Bem-Estar e prevenção de dores. Ajudo você a desenvolver hábitos mais saudáveis e sem dores no dia a dia. São mais de 30 certificações na área de massoterapia. Trabalho com massagens relaxantes, desportivas e terapêuticas, além de outras técnicas como terapia com ventosas, dry needling, moxa, cone hindu, pedras quentes e bandagem funcional.",
     },
     {
       title: "Psicóloga Regiane Magalhães (CRP 05/31197)",
-      image: "/assets/professional/PSI Regiane Magalhães.png",
+      image: "/assets/professional/profissional2.png",
       description:
         "Pós-graduada em Gestão de Pessoas e Psicanalista em formação. Já atuei como palestrante e professora universitária. Regularmente habilitada pelo Conselho de Psicologia, atualmente atuo como psicóloga clínica de adultos, auxiliando no tratamento de demandas emocionais e na promoção de Saúde e Bem-Estar.",
     },
     {
       title: "Psicóloga Bárbara Crivas (CRP 05/55030)",
-      image: "/assets/professional/PSI Bárbara Crivas.png",
+      image: "/assets/professional/profissional3.png",
       description:
         "Graduada em Psicologia e Direito. Atuo na promoção de Saúde e Bem-Estar com o público feminino no desenvolvimento de autonomia, resgate da autoestima e estabelecimento de relações saudáveis. Também atendo o público infantojuvenil por meio de grupos psicoterapêuticos associados ao trabalho com os familiares. Habilitada pelo Conselho de Psicologia para atendimentos presenciais e remotos.",
     },
     {
       title: "Psicóloga Raabe Santos (CRP 05/57226)",
-      image: "/assets/professional/PSI Raabe Santos.png",
+      image: "/assets/professional/profissional5.png",
       description:
         "Pós-graduada em Neuropsicologia, Psicopedagogia e Psicologia Hospitalar e da Saúde. Realizo avaliação do desenvolvimento infantil e trabalho com demandas de autismo infantil e técnica ABA visando a promoção de Saúde e Bem-Estar. Habilitada pelo Conselho de Psicologia, faço atendimento infantojuvenil e orientação de pais e responsáveis.",
     },
     {
       title: "Psicopedagoga Ana Cláudia",
-      image: "/assets/professional/PSI Ana Cláudia.png",
+      image: "/assets/professional/profissional4.png",
+      description:
+        "Professora de Educação Especial desde 2002 e formada em Psicopedagogia. Atende no Espaço Psicopedagógico em Barra do Piraí. Especialista em autismo, apaixonada por práticas baseadas em evidências: ABA, TEACCH, Denver, DIR Floortime (DIR 101 e 201 em andamento) e integração sensorial.",
+    },
+    {
+      title: "Psicóloga Fabaiana da Fonseca",
+      image: "/assets/professional/profissional6.png",
       description:
         "Professora de Educação Especial desde 2002 e formada em Psicopedagogia. Atende no Espaço Psicopedagógico em Barra do Piraí. Especialista em autismo, apaixonada por práticas baseadas em evidências: ABA, TEACCH, Denver, DIR Floortime (DIR 101 e 201 em andamento) e integração sensorial.",
     },
     {
       title: "Fisioterapeuta Andressa Flor",
-      image: "/assets/professional/FISIO Andressa Flor.png",
+      image: "/assets/professional/profissional7.png",
       description:
         "Fisioterapeuta com atuação em fisioterapia geral, estética, pilates, drenagem linfática, massagem modeladora e redutora. Parceira da ConsultAR, atende no Studio Mary & Flor Fisioterapia e com atendimento domiciliar.",
     },
